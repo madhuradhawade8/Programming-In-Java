48b5d16624cf606476decd447498c074f9a65f5b
madhura <madhura.dhawade24@gmail.com>
2025-05-21 12:26:46 +0530

    program2.java

---
+++ program2.java
@@ -0,0 +1,14 @@
+public class program2
+{
+    public static void main(String[] S)
+    {
+        int obtaied = 380;
+        int total = 400;
+        float percentage = 0.0f;
+
+        percentage = (obtaied * 100) / total;
+
+        System.out.println("Percentage is "+percentage);
+    }
+    
+}
